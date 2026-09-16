18d074a5737bc527a7dbffdd4b1a2a3c322e70f5
Frano Jančić <vidra74@gmail.com>
2026-09-16 14:11:28 +0200

    Update render_pdf.ps1

--- render_pdf.ps1
+++ render_pdf.ps1
@@ -1,1 +1,49 @@
+param(
+    [string]$PdfPath,
+    [string]$OutDir
+)
+
+$ErrorActionPreference = 'Stop'
+New-Item -ItemType Directory -Force -Path $OutDir | Out-Null
+
+Add-Type -AssemblyName System.Runtime.WindowsRuntime
+
+$asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | Where-Object { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+$asTaskAction = ([System.WindowsRuntimeSystemExtensions].GetMethods() | Where-Object { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncAction' })[0]
+
+function Await($WinRtTask, $ResultType) {
+    $asTask = $asTaskGeneric.MakeGenericMethod($ResultType)
+    $netTask = $asTask.Invoke($null, @($WinRtTask))
+    $netTask.Wait(-1) | Out-Null
+    $netTask.Result
+}
+
+function AwaitAction($WinRtAction) {
+    $netTask = $asTaskAction.Invoke($null, @($WinRtAction))
+    $netTask.Wait(-1) | Out-Null
+}
+
+[Windows.Data.Pdf.PdfDocument, Windows.Data.Pdf, ContentType = WindowsRuntime] | Out-Null
+[Windows.Storage.StorageFile, Windows.Storage, ContentType = WindowsRuntime] | Out-Null
+
+$file = Await ([Windows.Storage.StorageFile]::GetFileFromPathAsync($PdfPath)) ([Windows.Storage.StorageFile])
+$doc = Await ([Windows.Data.Pdf.PdfDocument]::LoadFromFileAsync($file)) ([Windows.Data.Pdf.PdfDocument])
+
+Write-Output "Pages: $($doc.PageCount)"
+
+$outFolder = Await ([Windows.Storage.StorageFolder]::GetFolderFromPathAsync($OutDir)) ([Windows.Storage.StorageFolder])
+
+for ($i = 0; $i -lt $doc.PageCount; $i++) {
+    $page = $doc.GetPage([uint32]$i)
+    $name = "page{0}.png" -f ($i + 1)
+    $storageFile = Await ($outFolder.CreateFileAsync($name, [Windows.Storage.CreationCollisionOption]::ReplaceExisting)) ([Windows.Storage.StorageFile])
+    $stream = Await ($storageFile.OpenAsync([Windows.Storage.FileAccessMode]::ReadWrite)) ([Windows.Storage.Streams.IRandomAccessStream])
+    $renderOptions = New-Object Windows.Data.Pdf.PdfPageRenderOptions
+    $renderOptions.DestinationWidth = [uint32]1600
+    $renderOptions.DestinationHeight = [uint32]([Math]::Round(1600 * $page.Size.Height / $page.Size.Width))
+    AwaitAction ($page.RenderToStreamAsync($stream, $renderOptions))
+    $stream.Dispose()
+    $page.Dispose()
+    Write-Output "Saved $name"
+}
 
